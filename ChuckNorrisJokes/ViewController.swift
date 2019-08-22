559661313febb53e661d3f6a9b630c388d48792f
Gabriel Henrique <p000gabriel.santos.pereira@prservicos.com.br>
2019-08-22 09:25:46 -0300

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Gabriel Henrique on 22/08/19.
+//  Copyright © 2019 Gabriel Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
